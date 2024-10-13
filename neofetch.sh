e0300c6fff79e54abb7fe566c143041f960bf0ef
katy-the-kat <hello@is-a.space>
2024-10-13 14:43:21 -0700

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -950,6 +950,19 @@ get_os() {
     esac
 }
 
+get_model() {
+    model="PowerEdge R6615"
+}
+#9754
+
+get_cpu() {
+    cpu="AMD EPYC 9754 (256) @ 2.249Ghz"
+}
+
+get_memory() {
+    ram="10MiB / 4718592MiB"
+}
+
 get_distro() {
     [[ $distro ]] && return
 
@@ -1207,14 +1220,6 @@ get_distro() {
         ascii_distro=$(trim "$distro")
 }
 
-get_model() {
-    model="PowerEdge R6615"
-}
-#9754
-
-get_cpu() {
-    cpu="AMD EPYC 9754 (256) @ 2.249Ghz"
-}
 
 get_title() {
     user=${USER:-$(id -un || printf %s "${HOME/*\/}")}
@@ -2136,146 +2141,6 @@ get_gpu() {
         gpu="${gpu/NVIDIA}"
         gpu="${gpu/Intel}"
     fi
-}
-
-get_memory() {
-    case $os in
-        "Linux" | "Windows")
-            # MemUsed = Memtotal + Shmem - MemFree - Buffers - Cached - SReclaimable
-            # Source: https://github.com/KittyKatt/screenFetch/issues/386#issuecomment-249312716
-            while IFS=":" read -r a b; do
-                case $a in
-                    "MemTotal") ((mem_used+=${b/kB})); mem_total="${b/kB}" ;;
-                    "Shmem") ((mem_used+=${b/kB}))  ;;
-                    "MemFree" | "Buffers" | "Cached" | "SReclaimable")
-                        mem_used="$((mem_used-=${b/kB}))"
-                    ;;
-                esac
-            done < /proc/meminfo
-
-            mem_used="$((mem_used / 1024))"
-            mem_total="$((mem_total / 1024))"
-        ;;
-
-        "Mac OS X" | "macOS" | "iPhone OS")
-            mem_total="$(($(sysctl -n hw.memsize) / 1024 / 1024))"
-            mem_wired="$(vm_stat | awk '/ wired/ { print $4 }')"
-            mem_active="$(vm_stat | awk '/ active/ { printf $3 }')"
-            mem_compressed="$(vm_stat | awk '/ occupied/ { printf $5 }')"
-            mem_compressed="${mem_compressed:-0}"
-            mem_used="$(((${mem_wired//.} + ${mem_active//.} + ${mem_compressed//.}) * 4 / 1024))"
-        ;;
-
-        "BSD" | "MINIX")
-            # Mem total.
-            case $kernel_name in
-                "NetBSD"*) mem_total="$(($(sysctl -n hw.physmem64) / 1024 / 1024))" ;;
-                *) mem_total="$(($(sysctl -n hw.physmem) / 1024 / 1024))" ;;
-            esac
-
-            # Mem free.
-            case $kernel_name in
-                "NetBSD"*)
-                    mem_free="$(($(awk -F ':|kB' '/MemFree:/ {printf $2}' /proc/meminfo) / 1024))"
-                ;;
-
-                "FreeBSD"* | "DragonFly"*)
-                    hw_pagesize="$(sysctl -n hw.pagesize)"
-                    mem_inactive="$(($(sysctl -n vm.stats.vm.v_inactive_count) * hw_pagesize))"
-                    mem_unused="$(($(sysctl -n vm.stats.vm.v_free_count) * hw_pagesize))"
-                    mem_cache="$(($(sysctl -n vm.stats.vm.v_cache_count) * hw_pagesize))"
-                    mem_free="$(((mem_inactive + mem_unused + mem_cache) / 1024 / 1024))"
-                ;;
-
-                "MINIX")
-                    mem_free="$(top -d 1 | awk -F ',' '/^Memory:/ {print $2}')"
-                    mem_free="${mem_free/M Free}"
-                ;;
-
-                "OpenBSD"*) ;;
-                *) mem_free="$(($(vmstat | awk 'END {printf $5}') / 1024))" ;;
-            esac
-
-            # Mem used.
-            case $kernel_name in
-                "OpenBSD"*)
-                    mem_used="$(vmstat | awk 'END {printf $3}')"
-                    mem_used="${mem_used/M}"
-                ;;
-
-                *) mem_used="$((mem_total - mem_free))" ;;
-            esac
-        ;;
-
-        "Solaris" | "AIX")
-            hw_pagesize="$(pagesize)"
-            case $os in
-                "Solaris")
-                    pages_total="$(kstat -p unix:0:system_pages:pagestotal | awk '{print $2}')"
-                    pages_free="$(kstat -p unix:0:system_pages:pagesfree | awk '{print $2}')"
-                ;;
-
-                "AIX")
-                    IFS=$'\n'"| " read -d "" -ra mem_stat <<< "$(svmon -G -O unit=page)"
-                    pages_total="${mem_stat[11]}"
-                    pages_free="${mem_stat[16]}"
-                ;;
-            esac
-            mem_total="$((pages_total * hw_pagesize / 1024 / 1024))"
-            mem_free="$((pages_free * hw_pagesize / 1024 / 1024))"
-            mem_used="$((mem_total - mem_free))"
-        ;;
-
-        "Haiku")
-            mem_total="$(($(sysinfo -mem | awk -F '\\/ |)' '{print $2; exit}') / 1024 / 1024))"
-            mem_used="$(sysinfo -mem | awk -F '\\/|)' '{print $2; exit}')"
-            mem_used="$((${mem_used/max} / 1024 / 1024))"
-        ;;
-
-        "IRIX")
-            IFS=$'\n' read -d "" -ra mem_cmd <<< "$(pmem)"
-            IFS=" " read -ra mem_stat <<< "${mem_cmd[0]}"
-
-            mem_total="$((mem_stat[3] / 1024))"
-            mem_free="$((mem_stat[5] / 1024))"
-            mem_used="$((mem_total - mem_free))"
-        ;;
-
-        "FreeMiNT")
-            mem="$(awk -F ':|kB' '/MemTotal:|MemFree:/ {printf $2, " "}' /kern/meminfo)"
-            mem_free="${mem/*  }"
-            mem_total="${mem/$mem_free}"
-            mem_used="$((mem_total - mem_free))"
-            mem_total="$((mem_total / 1024))"
-            mem_used="$((mem_used / 1024))"
-        ;;
-
-    esac
-
-    [[ "$memory_percent" == "on" ]] && ((mem_perc=mem_used * 100 / mem_total))
-
-    case $memory_unit in
-        gib)
-            mem_used=$(awk '{printf "%.2f", $1 / $2}' <<< "$mem_used 1024")
-            mem_total=$(awk '{printf "%.2f", $1 / $2}' <<< "$mem_total 1024")
-            mem_label=GiB
-        ;;
-
-        kib)
-            mem_used=$((mem_used * 1024))
-            mem_total=$((mem_total * 1024))
-            mem_label=KiB
-        ;;
-    esac
-
-    memory="${mem_used}${mem_label:-MiB} / ${mem_total}${mem_label:-MiB} ${mem_perc:+(${mem_perc}%)}"
-
-    # Bars.
-    case $memory_display in
-        "bar")     memory="$(bar "${mem_used}" "${mem_total}")" ;;
-        "infobar") memory="${memory} $(bar "${mem_used}" "${mem_total}")" ;;
-        "barinfo") memory="$(bar "${mem_used}" "${mem_total}")${info_color} ${memory}" ;;
-    esac
 }
 
 get_song() {
