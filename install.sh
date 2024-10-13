3837e7836f1c8c66288dc8ecbfae6db45d931ced
katy-the-kat <hello@is-a.space>
2024-10-13 15:27:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 DEBIAN_FRONTEND=noninteractive apt-get install -y dropbear wget
-wget -O /usr/bin/antiddos https://raw.githubusercontent.com/katy-the-kat/realinstallscript/refs/heads/main/anti-ddos
-wget -O https://raw.githubusercontent.com/katy-the-kat/cardedvpssetup/refs/heads/main/neofetch.sh
-chmod +x /usr/bin/anti-ddos
+wget -O /usr/bin/neofetch https://raw.githubusercontent.com/katy-the-kat/cardedvpssetup/refs/heads/main/neofetch.sh
 chmod +x /usr/bin/neofetch
 echo 'root:root' | chpasswd
 
